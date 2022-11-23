7c0cbe051c41c489232159a39ef9bed93155171c
MMJimenez <munozmiguel321@gmail.com>
2022-11-23 22:00:09 +0100

    table inflated

--- src/main/java/com/example/orderhibfx/MainTableController.java
+++ src/main/java/com/example/orderhibfx/MainTableController.java
@@ -1,12 +1,7 @@
 package com.example.orderhibfx;
 
-import com.example.orderhibfx.controllers.RequestTable;
 import com.example.orderhibfx.dao.RequestDAO;
 import com.example.orderhibfx.models.Request;
-import javafx.application.Application;
-import javafx.beans.Observable;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,7 +16,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -40,64 +34,42 @@ public class MainTableController implements Initializable {
     }
 
     @FXML
-    TableView<RequestTable> tableRequest;
+    private TableView<Request> tableRequest;
 
     @FXML
-    TableColumn columnClientName;
+    private TableColumn<Request, String> columnClientName;
 
     @FXML
-    TableColumn columnDate;
+    private TableColumn<Request, Date> columnDate;
 
     @FXML
-    TableColumn columnDelivered;
+    private TableColumn<Request, Integer> columnDelivered;
 
     @FXML
-    private TableColumn columnId;
+    private TableColumn<Request, Integer> columnId;
 
     @FXML
-    TableColumn columnProductName;
+    private TableColumn<Request, Integer> columnProductName;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        RequestDAO requestDAO = new RequestDAO();
-        ArrayList<Request> requests = requestDAO.getAll("");
-        ArrayList<RequestTable> requestsTable = new ArrayList<>();
-        for (Request request : requests) {
-            requestsTable.add(new RequestTable(request));
-        }
 
-        ObservableList<RequestTable> requestsObservableList = FXCollections.observableArrayList(requestsTable);
+        tableRequest.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
-
-//        tableRequest = new TableView<RequestTable>(requestsObservableList);
-//        //tableRequest.setEditable(false);
-//        tableRequest.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
-        //this.columnId = new TableColumn("id");
         columnId.setCellValueFactory(new PropertyValueFactory("id"));
+        columnDate.setCellValueFactory(new PropertyValueFactory("date"));
+        columnClientName.setCellValueFactory(new PropertyValueFactory("client"));
+        columnDelivered.setCellValueFactory(new PropertyValueFactory("delivered"));
+        columnProductName.setCellValueFactory(new PropertyValueFactory("product"));
 
-        columnDate = new TableColumn("date");
-        columnDate.setCellValueFactory(new PropertyValueFactory<RequestTable, String>("date"));
-
-        columnClientName = new TableColumn("client");
-        columnClientName.setCellValueFactory(new PropertyValueFactory<RequestTable, String>("client"));
-
-        columnDelivered = new TableColumn("delivered");
-        columnDelivered.setCellValueFactory(new PropertyValueFactory<RequestTable, String>("delivered"));
-
-        columnProductName = new TableColumn("name");
-        columnProductName.setCellValueFactory(new PropertyValueFactory<RequestTable, String>("name"));
-
-        tableRequest.setItems(requestsObservableList);
-        tableRequest.getColumns().addAll(columnId, columnDate, columnClientName, columnDelivered, columnProductName);
-
-
-        System.out.println(requestsObservableList.get(1).getId()
-                + "\n" + requestsObservableList.get(1).getDate().toString()
-                + "\n" + requestsObservableList.get(1).getClient()
-                + "\n" + requestsObservableList.get(1).getDelivered().toString()
-                + "\n" + requestsObservableList.get(1).getProduct().toString());
+        actualizarTabla();
     }
 
+    private void actualizarTabla() {
+        RequestDAO requestDAO = new RequestDAO();
+
+        tableRequest.getItems().clear();
+        tableRequest.getItems().addAll(requestDAO.getAll(""));
+    }
 
 }
